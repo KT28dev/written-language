bdbf478bd42b80a44add0a7eb8349c213e1ddc1c
KT28 <100810129+KT28dev@users.noreply.github.com>
2022-04-04 21:38:37 -0600

    fix typo

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const { translateText } = require('./translate')
 
-const text = "Moradin, the Dwarffather, is the dwarven lawful good chief deity. He represent the dwarven strength and force of will. He inspires dwarven inventions, seeks to improve the dwarven and encourage their good nature, intelligence, and harmonious existence with other good races while battling their pride and isolationist tendencies.";
+const text = "Moradin, the Dwarffather, is the dwarven lawful good chief deity. He represents the dwarven strength and force of will. He inspires dwarven inventions, seeks to improve the dwarven and encourage their good nature, intelligence, and harmonious existence with other good races while battling their pride and isolationist tendencies.";
 const characterId = 1;
 const fluency = 50;
 
